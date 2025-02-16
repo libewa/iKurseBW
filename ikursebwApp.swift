d824677b44a71b22afa956e633aadc0e09c0e420
Linus Warnatz <linus@libewa.xyz>
2025-02-16 18:21:52 +0100

    this should allow you to select the remaining courses for some reason, the previous views do not save the selection. will look into that

--- ikursebwApp.swift
+++ ikursebwApp.swift
@@ -12,6 +12,7 @@ struct ikursebwApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(CourseSelection(performerCourses: [], gradedBasicCourses: [], basicCourses: []))
         }
     }
 }
