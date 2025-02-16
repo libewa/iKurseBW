5e24c7d02ca57c182bb8a581ad5b1ec4ac3c8f89
Linus Warnatz <linus@libewa.xyz>
2025-02-16 16:25:16 +0100

    Initial commit with Xcode provided boilerplate

---
+++ ikursebw/ikursebwApp.swift
@@ -0,0 +1,17 @@
+//
+//  ikursebwApp.swift
+//  ikursebw
+//
+//  Created by Linus Warnatz on 15.02.25.
+//
+
+import SwiftUI
+
+@main
+struct ikursebwApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
