4183254e89d1110eb017e053fb50586c9f208fac
Linus Warnatz <linus@libewa.xyz>
2025-02-16 16:47:50 +0100

    remove FilePicker because it crashes on visionos

--- ikursebw/Views/GreetingView.swift
+++ ikursebw/Views/GreetingView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-import FilePicker
+//import FilePicker
 
 struct GreetingView: View {
     @State var availableCourses: [Course] = try! JSONDecoder().decode([Course].self, from: try! Data(contentsOf: Bundle.main.url(forResource: "courses", withExtension: "json")!))
@@ -14,17 +14,9 @@ struct GreetingView: View {
         VStack(alignment: .leading) {
             Text("Hallo, und willkommen bei iKurseBW!")
             Text("Diese App hilft dir bei der Wahl deiner Kurse für die Kursstufe des allgemeinbildenden Gymnasiums (Klasse 11 und 12 oder 12 und 13).")
-            Text("Bitte wähle eine Datei mit verfügbaren Kursen, oder benutze die Standardauswahl.")
-            FilePicker(types: [.json], allowMultiple: false, title: "Wähle eine Datei", onPicked: { urls in
-                if let url = urls.first {
-                    do {
-                        availableCourses = try JSONDecoder().decode([Course].self, from: try Data(contentsOf: url))
-                    } catch {
-                        print("Error decoding courses: \(error)")
-                    }
-                }
-            })
-            Text("Zuerst wählen wir deine Leistunskurse (LKs).")
+            /* Text("Bitte wähle eine Datei mit verfügbaren Kursen, oder benutze die Standardauswahl.")
+             FilePicker() */
+            Text("Zuerst wählen wir deine Leistungskurse (LKs).")
             NavigationLink("Weiter zur LK-Wahl", destination: PerformerCourseSelectionView(availableCourses: availableCourses))
         }
         .navigationTitle("Willkommen bei iKurseBW!")
